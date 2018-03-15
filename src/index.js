3e3d91982e5b63eb1d4bc7e6acc6c788c9b02084
VasiliyRepin <36683162+VasiliyRepin@users.noreply.github.com>
2018-03-15 23:11:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    function suggest(row,col) {
+        var result = [];
+        var freedom = {
+            row: Math.floor(row/3)*3,
+            col: Math.floor(col/3)*3,
+        };
+
+        for (var i = 0; i < 9; i++) {
+            result.push([matrix[row][i], matrix[i][col], matrix[freedom.row + i % 3][freedom.col + Math.floor(i / 3)]])
+        }
+        return result;
+    }
+
+    for (var row = 0; row < 9; row++) {
+        for (var col = 0; col < 9; col++) {
+            if (matrix[row][col] === 0) {
+                var suggestions = suggest(row, col);
+                for (var suggestion of suggestions) {
+                    matrix[row][col] = suggestion;
+                    solveSudoku(matrix);
+                }
+            }
+        }
+    }
+    return matrix;
 }
